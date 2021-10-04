8e525abaaf07f2c2922400c34c041792281c2288
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-04 15:01:26 -0300

    Estabelecer relação herança da classe Funcionário com a classe Pessoa

--- src/br/com/zup/Funcionario.java
+++ src/br/com/zup/Funcionario.java
@@ -1,4 +1,18 @@
 package br.com.zup;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
+    private String ctps;
+
+    public Funcionario(String nome, String cpf, String ctps) {
+        super(nome, cpf);
+        this.ctps = ctps;
+    }
+
+    public String getCtps() {
+        return ctps;
+    }
+
+    public void setCtps(String ctps) {
+        this.ctps = ctps;
+    }
 }
