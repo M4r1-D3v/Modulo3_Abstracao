85773735036298518d234823dcb5ff90475bff71
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-04 15:04:58 -0300

    Criar métodos construtor. getters/setters na classe Morador com relação de herança

--- src/br/com/zup/Morador.java
+++ src/br/com/zup/Morador.java
@@ -1,5 +1,18 @@
 package br.com.zup;
 
-public class Morador {
-    private String nome;
+public class Morador extends Pessoa {
+    private String telefone;
+
+    public Morador(String nome, String cpf, String telefone) {
+        super(nome, cpf);
+        this.telefone = telefone;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
