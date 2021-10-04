89c88d171733886c8bbd561b7fe8ba989d3dfc32
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-04 14:49:58 -0300

    Criar as classes e seus atributos

---
+++ src/br/com/zup/Imovel.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Imovel {
+    private String endereco;
+    private double valorAluguel;
+    private List <Morador>moradores = new ArrayList<>();
+}
---
+++ src/br/com/zup/Pessoa.java
@@ -0,0 +1,6 @@
+package br.com.zup;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+}
