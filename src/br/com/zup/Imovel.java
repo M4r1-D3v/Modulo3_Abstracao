1ffc04056aa813c83e0a1e7841b32dc05060f7d7
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-04 15:11:03 -0300

    Criar metodos construtores, getters/setters da classe Imovel

--- src/br/com/zup/Imovel.java
+++ src/br/com/zup/Imovel.java
@@ -6,5 +6,45 @@ import java.util.List;
 public class Imovel {
     private String endereco;
     private double valorAluguel;
+    private Funcionario funcionarioResponsavel;
     private List <Morador>moradores = new ArrayList<>();
+
+    public Imovel(String endereco, double valorAluguel, Funcionario funcionarioResponsavel) {
+        this.endereco = endereco;
+        this.valorAluguel = valorAluguel;
+        this.funcionarioResponsavel = funcionarioResponsavel;
+
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getValorAluguel() {
+        return valorAluguel;
+    }
+
+    public void setValorAluguel(double valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
+
+    public Funcionario getFuncionarioResponsavel() {
+        return funcionarioResponsavel;
+    }
+
+    public void setFuncionarioResponsavel(Funcionario funcionarioResponsavel) {
+        this.funcionarioResponsavel = funcionarioResponsavel;
+    }
+
+    public List<Morador> getMoradores() {
+        return moradores;
+    }
+
+    public void setMoradores(List<Morador> moradores) {
+        this.moradores = moradores;
+    }
 }
