c6455e91de7fe018de1cb4c90f8a4c6483f6bbf5
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-04 15:48:26 -0300

    Criar interface com métodos excluirMoradorEndereco, excluirMoradorCPF, proibirCpfRepetido

--- src/br/com/zup/ModeloImovel.java
+++ src/br/com/zup/ModeloImovel.java
@@ -1,4 +1,12 @@
 package br.com.zup;
 
 public interface ModeloImovel {
+    //implementar excluir morador pelo endereço do imovel e cpf do morador
+    //implementar metodo que não permite adicionar um morador com cpf repetido
+
+    public abstract void excluirMoradorEndereco(Morador moradorExcluido);
+    public abstract void excluirMoradorCPF(Morador moradorExcluido);
+    public abstract void proibirCpfRepetido(String cpf);
+
+
 }
